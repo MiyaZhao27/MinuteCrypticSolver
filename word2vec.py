cd7e6aab91c6fcab24ebb0fde928f14015ac5597
amandahuang2818 <96457142+amandahuang2818@users.noreply.github.com>
2025-11-28 18:59:16 -0500

    word2vec without the actual file that it needs unfortunately.

--- word2vec.py
+++ word2vec.py
@@ -1,1 +1,15 @@
+import sys
+import numpy as np
+from gensim.models import KeyedVectors
 
+model = None
+
+def get_model():
+    global model
+    if model is None:
+        print("Loading Word2Vec model....")
+        model = KeyedVectors.load(
+            "/Users/amandahuang/Desktop/MinuteCrypticSolver/word2vec-google-news-300.model",
+            mmap="r"
+        )
+    return model
